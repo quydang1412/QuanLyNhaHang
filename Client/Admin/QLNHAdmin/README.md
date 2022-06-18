3424c27e9a6f45a634e617837c2c8fabf921af9f
quydang1412 <quydang1412@gmail.com>
2022-06-19 06:17:17 +0700

    PrimeNgDone

--- Client/Admin/QLNHAdmin/README.md
+++ Client/Admin/QLNHAdmin/README.md
@@ -1,6 +1,6 @@
 # QLNHAdmin
 
-This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.0.
+This project was generated with [Angular CLI](https://github.com/angular/angular-cli) version 14.0.2.
 
 ## Development server
 
--- Client/Admin/QLNHAdmin/src/app/app.module.ts
+++ Client/Admin/QLNHAdmin/src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import {MenubarModule} from 'primeng/menubar';
+import {AccordionModule} from 'primeng/accordion';
+import {InputTextModule} from 'primeng/inputtext';
+import {ButtonModule} from 'primeng/button';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MenubarModule,
+    AccordionModule, 
+    InputTextModule, 
+    ButtonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
